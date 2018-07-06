f6b12b969e6474b48d8dc226da4f7f3fedd09501
Danny.Wang <danny_wang@gomro.cn>
2018-07-06 13:55:44 +0800

    MOD:复杂查询

--- core/src/main/java/com/sncj/core/student/repository/IStudentRepository.java
+++ core/src/main/java/com/sncj/core/student/repository/IStudentRepository.java
@@ -2,9 +2,10 @@ package com.sncj.core.student.repository;
 
 import com.sncj.core.student.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * Created by Danny on 2018/7/6.
  */
-public interface IStudentRepository extends JpaRepository<StudentEntity, Integer> {
+public interface IStudentRepository extends JpaRepository<StudentEntity, Integer>,JpaSpecificationExecutor<StudentEntity> {
 }
--- core/src/main/java/com/sncj/core/student/service/impl/StudentService.java
+++ core/src/main/java/com/sncj/core/student/service/impl/StudentService.java
@@ -1,13 +1,18 @@
 package com.sncj.core.student.service.impl;
 
 import com.sncj.core.baseconfig.BasePage;
+import com.sncj.core.baseconfig.RegexUtils;
 import com.sncj.core.student.entity.StudentEntity;
 import com.sncj.core.student.repository.IStudentRepository;
 import com.sncj.core.student.service.IStudentService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Danny on 2018/7/6.
@@ -17,8 +22,20 @@ public class StudentService implements IStudentService {
     @Resource
     private IStudentRepository iStudentRepository;
 
+    private Specification<StudentEntity> condition(StudentEntity student) {
+
+        return (root, query, cb) -> {
+
+            List<Predicate> predicates = new ArrayList<>();
+            if (null != student) {
+                if (RegexUtils.notNull(student.getName())) predicates.add(cb.equal(root.get("name"), student.getName()));
+            }
+            return cb.and(predicates.toArray(new Predicate[predicates.size()]));
+        };
+    }
+
     @Override
     public Page<StudentEntity> findAll(BasePage basePage, StudentEntity studentEntity) {
-        return iStudentRepository.findAll(basePage.getRequestPage());
+        return iStudentRepository.findAll(condition(studentEntity),basePage.getRequestPage());
     }
 }
